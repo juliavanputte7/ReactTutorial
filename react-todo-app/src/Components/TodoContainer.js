4b3476fa7ab47a0c1418918620fe53fd8e10334e
juliavanputte7 <jhv42@cornell.edu>
2021-01-24 18:32:38 -0500

    redo react bc it wasnt working

--- react-todo-app/src/Components/TodoContainer.js
+++ react-todo-app/src/Components/TodoContainer.js
@@ -22,15 +22,14 @@ class TodoContainer extends React.Component {
         ]
     };
     handleChange = (id) => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
+        this.setState(prevState => ({
+            todos: prevState.todos.map(todo => {
                 if (todo.id == id) {
                     todo.completed = !todo.completed;
                 }
                 return todo;
             })
-        })
-        console.log("clicked", id);
+        }))
     };
     render() {
         return (
---
+++ react-todo-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./Components/TodoContainer"
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
