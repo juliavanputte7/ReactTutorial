f59e1e1f83180996351c14028f8ea44d3dccdde7
juliavanputte7 <jhv42@cornell.edu>
2021-01-25 10:04:12 -0500

    function vs class based

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./Components/TodoContainer"
+import TodoContainer from "./functionBased/Components/TodoContainer"
 
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
+
+import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
     <React.StrictMode>
-      <TodoContainer />
+        <BrowserRouter>
+            <TodoContainer />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
   )
