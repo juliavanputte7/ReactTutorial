0ad98547ffd20e709b192c0f9580c8482ffd9ed3
juliavanputte7 <jhv42@cornell.edu>
2021-01-24 19:52:30 -0500

    react updates

--- react-todo-app/src/Components/TodosList.js
+++ react-todo-app/src/Components/TodosList.js
@@ -9,6 +9,8 @@ class TodosList extends React.Component {
                         key={todo.id} 
                         todo={todo} 
                         handleChangeProps={this.props.handleChangeProps}
+                        deleteTodoProps={this.props.deleteTodoProps}
+                        setUpdate={this.props.setUpdate}
                     />
                 ))}
             </ul>
--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -2,4 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./Components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+
+//stylesheet
+import "./App.css"
+
+ReactDOM.render(
+    <React.StrictMode>
+      <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
